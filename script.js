cdfb70a3321eb0c689c41ea3afa0d63114a87481
shaksworld <shakaaliakwe@gmail.com>
2023-03-06 01:01:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+    strings: ["Software Engineer", "Web Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
